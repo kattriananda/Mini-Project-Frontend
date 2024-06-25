4de20d00c145fe6d42e7a09006d51aff297f18b0
kattriananda <kattriananda@gmail.com>
2024-06-25 21:41:39 +0700

    penambahan roote

--- mini-project/src/App.jsx
+++ mini-project/src/App.jsx
@@ -1,30 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './page/Home'
-import Pembayaran from './page/Pembayaran'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import ListProduct from './page/ListProduct'
-import ProductCrud from './page/ProductCrud'
-import Transaksi from './page/Transaksi'
+import { useState } from "react";
+import "./App.css";
+import Home from "./page/Home";
+import Pembayaran from "./page/Pembayaran";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ListProduct from "./page/ListProduct";
+import ProductCrud from "./page/ProductCrud";
+import Transaksi from "./page/Transaksi";
+import CategoryCrud from "./page/CategoryCrud";
 
 function App() {
-  // const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
-  return (
-    <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/listproduct' element={<ListProduct/>}/>
-        <Route path='/checkout' element={<Pembayaran/>}/>
-        <Route path='/product' element={<ProductCrud/>}/>
-        <Route path='/transaksi' element={<Transaksi/>}/>
-      </Routes>
-    </Router>
-      {/* <Home/> */}
-      {/* <Pembayaran/> */}
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/listproduct" element={<ListProduct />} />
+                    <Route path="/checkout" element={<Pembayaran />} />
+                    <Route path="/product" element={<ProductCrud />} />
+                    <Route path="/transaksi" element={<Transaksi />} />
+                    <Route path="/category" element={<CategoryCrud />} />
+                </Routes>
+            </Router>
+            {/* <Home/> */}
+            {/* <Pembayaran/> */}
+        </>
+    );
 }
 
-export default App
+export default App;
